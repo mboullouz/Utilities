87cec28255521b4a400c761a1d0ff0408f21121e
mohamedb <mohamed.boullouz@gmail.com>
2016-03-09 23:28:59 +0100

    edit Action class to match Json file

--- Utilities/src/main/java/sf/generator/ctrl/Action.java
+++ Utilities/src/main/java/sf/generator/ctrl/Action.java
@@ -6,25 +6,36 @@
 package sf.generator.ctrl;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
- *
+ * JSon equivalent
+ *{
+   "name":"a",
+   "viewUrl":"u/r/l",
+   "params" : ["p1","p2"],
+   "arrayReturn":{"m1": "a", "m2": "b"}
+   
+ }
  * @author mohamed
  */
 public class Action {
 
     private String name;
-    private Set<String> params;
     private String viewUrl;
+    private List<String> params;
     private Map<String, String> arrayReturn;
 
-    public Action(String name, String viewUrl, Set<String> params) {
+    public Action(String name, String viewUrl, List<String> params) {
         this.name = name;
         this.viewUrl = viewUrl;
         this.params = params;
         this.arrayReturn = new HashMap<>();
+    }
+
+    public Action() {
+
     }
 
     public void addParam(String p) {
@@ -35,7 +46,36 @@ public class Action {
         this.arrayReturn.put(key, value);
     }
 
-    
- 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getParams() {
+        return params;
+    }
+
+    public void setParams(List<String> params) {
+        this.params = params;
+    }
+
+    public String getViewUrl() {
+        return viewUrl;
+    }
+
+    public void setViewUrl(String viewUrl) {
+        this.viewUrl = viewUrl;
+    }
+
+    public Map<String, String> getArrayReturn() {
+        return arrayReturn;
+    }
+
+    public void setArrayReturn(Map<String, String> arrayReturn) {
+        this.arrayReturn = arrayReturn;
+    }
 
 }
