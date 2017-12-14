57a38f457afe46905eecca7fcbd13ae1cd3008ff
mboullouz <mohamedboullouz@gmail.com>
2017-12-14 22:43:44 +0100

    some progress

--- utilites/src/main/java/game/card/ExpressionEvaluator.java
+++ utilites/src/main/java/game/card/ExpressionEvaluator.java
@@ -3,6 +3,7 @@ package game.card;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Stack;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -19,21 +20,116 @@ public class ExpressionEvaluator {
         return parse(in, operatorPredicate(), String::toString);
     }
 
-    public static Integer doMath(Stack<Integer> numbers, Stack<String> operators) {
+    public static Integer doOp(Integer left, Integer right, String op) {
+        if (op.contains("+"))
+            return left + right;
+        if (op.contains("*"))
+            return left * right;
+        return 0;
+    }
+
+    public static SResult doMath(Stack<Integer> numbers, Stack<String> operators) {
         Integer result = 0;
-        Integer left = 0;
-        Integer right = 0;
-        while (!numbers.empty()) {
-            left = numbers.pop();
-            if (numbers.empty())
-                right = 0;
-            else
-                right = numbers.pop();
-            result += left + right;
-            if (!operators.empty())
-                operators.pop();
+        Stack<Integer> rs = new Stack<>();
+        Stack<String> ops = new Stack<>();
+        while (!operators.empty()) {
+            String op = operators.pop();
+            if (op.contains(")")) {
+                op = operators.pop();
+                boolean f = false;
+                while (!op.contains("(")) {
+                    f = true;
+                    op = operators.pop();
+                    Integer r = numbers.pop();
+                    Integer l = numbers.pop();
+                    numbers.push(r + l);
+                }
+                if (f) {
+                    rs.push(numbers.pop());
+                    if(operators.size()>0)
+                        ops.push(operators.pop());
+                    return new SResult(rs, ops).other(numbers, operators);
+                }
+            }
+
         }
-        return result;
+        return null;
+    }
+
+
+    public static class SResult {
+        public Stack<Integer> rs = new Stack<>();
+        public Stack<String> ops = new Stack<>();
+
+        public Stack<Integer> otherRs = new Stack<>();
+        public Stack<String> otherOps = new Stack<>();
+
+        public SResult(Stack<Integer> rs, Stack<String> ops) {
+            this.rs = rs;
+            this.ops = ops;
+        }
+
+        SResult other(Stack<Integer> rs, Stack<String> ops) {
+            this.otherRs = rs;
+            this.otherOps = ops;
+            return this;
+        }
+
+        SResult merge(SResult other){
+            rs.addAll(other.rs);
+            ops.addAll(other.ops);
+            return this;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+
+            if (o == null || getClass() != o.getClass()) return false;
+            SResult sResult = (SResult) o;
+            if (sResult.rs.size() != this.rs.size()
+                    || sResult.ops.size() != this.ops.size()
+                    ) {
+                return false;
+            }
+
+            System.out.println(" ....new..... ");
+
+            for (int i = 0; i < sResult.rs.size(); i++) {
+                System.out.println(sResult.rs.toArray()[i] + " rs: " + this.rs.toArray()[i]);
+                if (!sResult.rs.toArray()[i].equals(this.rs.toArray()[i])) {
+                    return false;
+                }
+            }
+            System.out.println("---------");
+            for (int i = 0; i < sResult.ops.size(); i++) {
+                System.out.println(sResult.ops.toArray()[i] + " ops: " + this.ops.toArray()[i]);
+                if (!sResult.ops.toArray()[i].equals(this.ops.toArray()[i])) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(rs, ops);
+        }
+    }
+
+    public static Stack<String> pass(Stack<String> s) {
+        while (!s.empty()) {
+            String e = s.pop();
+            if (isOperator(e)) {
+                if (e.contains(")")) {
+                    e = s.pop();
+                    while (!e.contains("(")) {
+
+                    }
+                }
+            }
+        }
+        return null;
     }
 
     public static Predicate<String> operatorPredicate() {
@@ -53,7 +149,6 @@ public class ExpressionEvaluator {
                 r += c + "";
             }
         }
-        System.out.println(r);
         final String[] els = r.trim().split(" +");
         List<T> nums = Stream.of(els).filter(predicate).map(mapper).collect(Collectors.toList());
         Stack<T> stack = new Stack<>();
--- utilites/src/test/java/games/ExpressionEvalTest.java
+++ utilites/src/test/java/games/ExpressionEvalTest.java
@@ -10,21 +10,46 @@ public class ExpressionEvalTest {
 
     @Test
     public void stringTo2Stacks() {
-        String in = "(1 + 2)+(2+5)";
+        String in = "(1+2+3+4)*(4+6)";
         Stack<Integer> exp = new Stack<>();
         exp.push(1);
         exp.push(2);
-       // Assert.assertEquals(exp, ExpressionEvaluator.numbers(in));
+        // Assert.assertEquals(exp, ExpressionEvaluator.numbers(in));
 
         Stack<String> expSymbols = new Stack<>();
         expSymbols.push("(");
         expSymbols.push("+");
         expSymbols.push(")");
-       // Assert.assertEquals(expSymbols, ExpressionEvaluator.operators(in));
+        // Assert.assertEquals(expSymbols, ExpressionEvaluator.operators(in));
 
-        Assert.assertEquals(new Integer(10), ExpressionEvaluator.doMath(
+       /* Assert.assertEquals(new Integer(20), ExpressionEvaluator.doMath(
                 ExpressionEvaluator.numbers(in),
                 ExpressionEvaluator.operators(in)
+        )); */
+        Assert.assertEquals(new ExpressionEvaluator.SResult(
+                ExpressionEvaluator.numbers("(1+2+3+4)*(4+6)"),
+                ExpressionEvaluator.operators(in)
+        ), new ExpressionEvaluator.SResult(
+                ExpressionEvaluator.numbers(in),
+                ExpressionEvaluator.operators(in)
+        ));
+        ExpressionEvaluator.SResult p1 = ExpressionEvaluator.doMath(
+                ExpressionEvaluator.numbers("(1+2+3+4)*(4+6)"),
+                ExpressionEvaluator.operators(in)
+        );
+        Assert.assertEquals(p1, new ExpressionEvaluator.SResult(
+                ExpressionEvaluator.numbers("10"),
+                ExpressionEvaluator.operators("*")
+        ));
+
+        ExpressionEvaluator.SResult p2 = ExpressionEvaluator.doMath(
+                p1.otherRs,
+                p1.otherOps
+        );
+
+        Assert.assertEquals(p2, new ExpressionEvaluator.SResult(
+                ExpressionEvaluator.numbers("10"),
+                ExpressionEvaluator.operators("")
         ));
 
     }
